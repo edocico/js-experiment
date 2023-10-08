925f71506c232ac8fadd0f1c071782e1f9191345
Edocico <edoardo.cico@gmail.com>
2023-10-08 15:31:09 +0200

    js get elements by DOM

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 console.log('hello world')
+
+// recupero elementi del DOM
+
+const okBtnDOMElement = document.getElementById('ok-btn')
+
+const inputDOMElement = document.getElementById('n-input')
+
+const cardF1DOMElement = document.querySelector('.card-front1')
+
+const cardB1DOMElement = document.querySelector('card-back1')
+
+const cardF2DOMElement = document.querySelector('.card-front2')
+
+const cardB2DOMElement = document.querySelector('card-back2')
+
+const cardF3DOMElement = document.querySelector('.card-front3')
+
+const cardB3DOMElement = document.querySelector('card-back3')
